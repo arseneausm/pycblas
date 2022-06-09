fd6449c532881ab5c3c77ca20894b003045654ce
Stefan <arseneausm@gmail.com>
2022-06-08 21:37:46 -0400

    reorganized

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def main():
         description = "Wrapper for CBLAS in Python",
         author = "Stefan Arseneau",
         author_email = "sarsene1@jhu.edu",
-        ext_modules = [Extension("pycblas", ["matrixop.c"])])
+        ext_modules = [Extension("pycblas", ["src/matrixop.c"])])
 
 if __name__ == "__main__":
 	main()
